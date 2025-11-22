035ef04df231af6aaa8fd194684d001dd0345866
Christian <demco21@gmail.com>
2025-11-22 12:56:06 -0500

    change to 1 hour for pvp disabled

--- services/pvp_service.py
+++ services/pvp_service.py
@@ -30,14 +30,14 @@ class PVPService:
         await ctx.author.add_roles(pvp_disabled_role)
         self.state.pvp_disabled_members[ctx.author.id] = datetime.now()
         logger.info(f"Disabled PVP {self.state.pvp_disabled_members}")
-        await ctx.send(f"PVP has been disabled for {self.get_name(ctx.author)} and will be re-enabled in 20 minutes.")
+        await ctx.send(f"PVP has been disabled for {self.get_name(ctx.author)} and will be re-enabled in 1 hour.")
 
     async def check_reenable_pvp(self):
         try:
             now = datetime.now()
             to_reenable = []
             for member_id, disabled_time in self.state.pvp_disabled_members.items():
-                if now - disabled_time >= timedelta(minutes=20):
+                if now - disabled_time >= timedelta(hours=1):
                     to_reenable.append(member_id)
 
             for member_id in to_reenable:
--- services/time_service.py
+++ services/time_service.py
@@ -17,7 +17,6 @@ from constants import (
 from config import (
     AFK_CHANNEL_ID,
     ANNOUNCEMENTS_CHANNEL_ID,
-    CHANCELLOR_ROLE_ID,
     HOGBOT_SERVER_ID
 )
 from logging_config import logger
@@ -306,58 +305,5 @@ class TimeService:
             if key in timestamps:
                 self.pop_timestamp_and_calculate(key)
                 timestamps[key] = datetime.now()
-
-    async def decide_chancellor(self):
-
-        def clear_this_week_time_sums():
-                self.state.this_week_time_sums.clear()
-        
-        try:
-            this_week_time_sums = self.state.this_week_time_sums
-            channel = self.bot.get_channel(ANNOUNCEMENTS_CHANNEL_ID)
-            if not channel:
-                logger.warning('Channel not found')
-                return
-            
-            logger.info('Channel found')
-            ctx_message = await channel.send('A new Chancellor is to be appointed...')
-            ctx = await self.bot.get_context(ctx_message, cls=commands.Context)
-            
-            self.reset_active_timestamps(ctx.guild)
-            sorted_times = await self.time_spent_all_members(ctx, this_week_time_sums)
-            
-            if not sorted_times:
-                logger.info('No sorted times found')
-                return
-            
-            chancellor = sorted_times[0]
-            chancellor_id = chancellor[0].replace(KEY_SUFFIX_VOICE, '')
-            logger.info(f'Chancellor ID found, announcing winner: {chancellor_id}')
-            await self.appoint_chancellor(ctx, chancellor_id)
-            clear_this_week_time_sums()
-        except Exception as e:
-            logger.error(f"Error in decide_chancellor: {e}")
-
-    async def appoint_chancellor(self, ctx, member_id):
-
-        async def remove_role_for_all(ctx, role):
-            for member in ctx.guild.members:
-                if role in member.roles:
-                    await member.remove_roles(role)
-
-        current_chancellor_id = self.state.current_chancellor_id
-        member = ctx.guild.get_member(int(member_id))
-        if member:
-            logger.info(f'chancellor id: {CHANCELLOR_ROLE_ID}')
-            chancellor = ctx.guild.get_role(CHANCELLOR_ROLE_ID)
-            if chancellor is None:
-                logger.info('Chancellor role not found!')
-            else:
-                await remove_role_for_all(ctx, chancellor)
-                await member.add_roles(chancellor)
-                current_chancellor_id = member.id
-                await ctx.send(f'ALL HAIL OUR NEW CHANCELLOR, {self.get_name(member)} !')
-        else:
-            await ctx.send('No Chancellor found.')
 
 __all__ = ['TimeService']
