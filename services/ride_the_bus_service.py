3284b697e8acc12a8962ab24d330dbd7fcf879cd
Christian <demco21@gmail.com>
2025-12-04 23:37:49 -0500

    fix screen 1 bug

--- services/ride_the_bus_service.py
+++ services/ride_the_bus_service.py
@@ -103,7 +103,7 @@ class RideTheBusView(discord.ui.View):
             embed.add_field(name="Cashout Value", value=f"🪙 0", inline=True)
             embed.add_field(name="Balance", value=f"🪙 {self.state.member_wallets[self.player.id]}", inline=True)
         # first round, lost
-        if self.stage == 1 and win is not True and game_over is True:
+        elif self.stage == 1 and win is not True and game_over is True:
             embed.add_field(name="Final Payout", value=f"🪙 0", inline=True)
             embed.add_field(name="Balance", value=f"🪙 {self.state.member_wallets[self.player.id]}", inline=True)
         # ongoing game, show cashout value
